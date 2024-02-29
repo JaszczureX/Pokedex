663ebced29702c1afb4a5f2b4947b8cf748d4a77
JaszczureX <116756583+JaszczureX@users.noreply.github.com>
2024-02-29 10:24:50 +0100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,36 +1,39 @@
-const gameRouletteButton = document.getElementById("game1");
 const AppScreen = document.getElementById("app");
 
 const RouletteAtributes = [
     {
-        Id: 1,
+        Id: 0,
         headerContent: 'MainMenu',
         div2Content: '',
     }
-]
+];
 
 const RenderGames = () => {
     AppScreen.innerHTML = `
-    <div class="chooseGame-button" id="game1">
-        Gra 1
-    </div>
-    <div class="chooseGame-button" id="game2">
-        Gra 2
-    </div>
-    <div class="chooseGame-button" id="game3">
-        Gra 3
-    </div>
+        <div id="bufor">
+            <div class="chooseGame-button" id="game1">
+                Gra 1
+            </div>
+            <div class="chooseGame-button" id="game2">
+                Gra 2
+            </div>
+            <div class="chooseGame-button" id="game3">
+                Gra 3
+            </div>
+        </div>
     `;
-}
+};
 
 const Roulette = () => {
-    AppScreen.className = "appGame";
-    gameRouletteButton.addEventListener("click", () =>{
+    const gameRouletteButton = document.getElementById("game1");
+    gameRouletteButton.addEventListener("click", () => {
 
     })
-}
+};
 
-const Game = () =>{
+const Game = () => {
     RenderGames();
     Roulette();
-}
+};
+
+document.addEventListener("DOMContentLoaded", Game);
